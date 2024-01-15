ffdf11c6204baef09a35f2562260e421ffb009b9
Andrii Chuzha <77801608+panpanzerschreck@users.noreply.github.com>
2024-01-15 16:38:13 +0200

    Add files via upload

---
+++ V16/src/VkladList.java
@@ -0,0 +1,3 @@
+public class VkladList{
+    void 
+}// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ V16/src/vklad.java
@@ -0,0 +1,7 @@
+public class vklad{
+public static String Bank;
+public static boolean EarlyWithdraw;
+public static boolean TopUp;
+public static int Term;
+public static float Interest;
+}//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
